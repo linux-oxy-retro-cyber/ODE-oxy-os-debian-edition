b6074527d882f638e8dd504c9f146dba6596aba4
Gabriel Ramos Barbosa <apeludoff120@gmail.com>
2026-08-17 02:34:50 -0300

    Delete gemini-code-1786940043635.sh

--- gemini-code-1786940043635.sh
+++
@@ -1,9 +0,0 @@
-# Cria as pastas necessárias no skel se não existirem
-mkdir -p /etc/skel/.config/dconf
-mkdir -p /etc/skel/.config/fastfetch
-
-# Copia o banco de dados do GNOME/DConf
-cp -f ~/.config/dconf/user /etc/skel/.config/dconf/
-
-# Copia a config do fastfetch (caso esteja na sua home)
-cp -rf ~/.config/fastfetch/* /etc/skel/.config/fastfetch/
